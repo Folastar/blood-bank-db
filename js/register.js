45c0b49bbf0cc0949bed7951dead5a6992ddce08
Folastar <ogundelefolariin@gmail.com>
2025-07-29 16:59:37 +0100

    login and reg page

---
+++ js/register.js
@@ -0,0 +1,29 @@
+const RegisterBtn =document.getElementById('register-submit')
+
+// console.log(UserName)
+
+
+RegisterBtn.addEventListener('submit',async(e)=>{
+    e.preventDefault()
+    const UserName= document.querySelector('#register-username').value
+    const Password= document.querySelector('#register-password').value
+    const Email= document.querySelector('#register-email').value
+    try{
+
+        const res= await axios.post(" ",{
+         UserName,
+         Email,
+         Password
+        })
+
+        console.log(res.data)
+    }
+
+   catch(error){
+        console.error("error", err.message)
+   }
+})
+
+
+
+console.log("djfdkfjdkfjdkj")
